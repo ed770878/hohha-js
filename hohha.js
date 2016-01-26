71b5eba8abcb5061493b09e6a2de3e1707df6eca
Oscar <hi7ls@slipry.net>
2016-01-26 12:00:00 +0000

    Give credit where due (fix licenses & attribution)

--- hohha.js
+++ hohha.js
@@ -1,3 +1,5 @@
+// This is free and unencumbered software released into the public domain.
+
 // This could be cleaned up and modularized...
 
 //----------------------------------------------------------------------
